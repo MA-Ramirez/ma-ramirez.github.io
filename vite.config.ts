15220e3a6862260695d7ea43713c6e770570f45c
María Alejandra Ramírez <ramirez@evolbio.mpg.de>
2026-03-09 18:55:04 +0100

    Changes for gh deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ma-ramirez.github.io/",
+
   server: {
     host: "::",
     port: 8080,
